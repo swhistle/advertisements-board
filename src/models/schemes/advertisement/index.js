120c0850f7ac935ac3f0c4708bf01c55b4fa7569
Aleksandr Svistunov <aleksandr.svistunov@quadcode.com>
2021-11-18 11:42:04 +0300

    improved project structure, used express router, added api get adv by id

---
+++ src/models/schemes/advertisement/index.js
@@ -0,0 +1,26 @@
+const {Schema, model} = require('mongoose');
+
+const advertisementScheme = Schema(
+    {
+        shortText: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        toJSON: {
+            transform: (doc, ret) => {
+                ret.id = ret._id;
+                delete ret._id;
+                delete ret.__v;
+            }
+        }
+    }
+
+    );
+
+module.exports = model('Advertisement', advertisementScheme);
